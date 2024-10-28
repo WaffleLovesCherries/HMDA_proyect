3538e3f7b8873acb476df12ca75471bf1e7517dd
WaffleLovesCherries <95989576+WaffleLovesCherries@users.noreply.github.com>
2024-10-28 00:42:01 -0500

    Update App.py

--- App.py
+++ App.py
@@ -39,4 +39,4 @@ load_base_callbacks( app=app )
 
 # Server
 if __name__ == '__main__':
-    app.run_server( debug=True )
+    app.run_server( debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)) )
